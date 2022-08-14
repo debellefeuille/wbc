bfda779dcb68a7454accde95eda7613b32cd3bc0
Francois de Bellefeuille <francois@debellefeuille.ninja>
2022-08-14 16:24:38 -0400

    Add Material UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Typography } from '@mui/material';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Typography variant="h1" component="h2">
+        h1. Heading
+      </Typography>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp
