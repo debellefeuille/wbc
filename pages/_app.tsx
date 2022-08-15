c7e6cd487242181b9d773612e931adb741a375d0
Francois de Bellefeuille <francois@debellefeuille.ninja>
2022-08-14 23:36:56 -0400

    Menu Bar, not finish

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,57 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { Typography } from '@mui/material';
+import { Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import { Box } from '@mui/system';
+import { SxProps } from '@mui/system';
+import { Mail, MoveToInbox } from '@mui/icons-material';
 import AppTopBar from '../components/AppTopBar';
+import React from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [menuStatus, setMenuStatus] = React.useState(true);
+  const sxBar:SxProps = { width: 250 };
+
   return (
-    <div>
+    <React.Fragment>
       <AppTopBar></AppTopBar>
+      <Drawer
+        anchor="left"
+        open={menuStatus}
+      >
+        <Box
+          sx={{width:250}}
+          role="presentation"
+        >
+          <List>
+            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <Mail />
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {['All mail', 'Trash', 'Spam'].map((text) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <Mail />
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        
+      </Drawer>
       <Component {...pageProps} />
-    </div>
+    </React.Fragment>
   );
 }
 
